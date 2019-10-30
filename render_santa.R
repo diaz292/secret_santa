406352c72d9ce0d3f3d34a7ef421a0e43f592423
diaz292 <diaz292@gmail.com>
2019-10-29 22:13:16 -0400

    update seed and add option to omit matches

--- render_santa.R
+++ render_santa.R
@@ -2,10 +2,13 @@ library(knitr)
 
 year <- 2019
 limit <- 15
-
+run_matches <- "no"
 
 rmarkdown::render((here::here("template", "secret_santa.Rmd")),
                   output_dir = here::here("docs"),
-                  params = list(year = year
-                                , limit = limit),
+                  params = list(year = year,
+                                limit = limit,
+                                run_matches = run_matches
+                                ),
                   output_file = paste0("secret_santa_", year))
+
