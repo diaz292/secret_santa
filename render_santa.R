d8f54e8ca5c2117fdc44096b9b50ab42b825daa9
David Diaz <diaz292@gmail.com>
2019-11-08 21:00:26 -0500

    put params into function call

--- render_santa.R
+++ render_santa.R
@@ -1,14 +1,10 @@
 library(knitr)
 
-year <- 2019
-limit <- 15
-run_matches <- "no"
-
 rmarkdown::render((here::here("template", "secret_santa.Rmd")),
                   output_dir = here::here("docs"),
-                  params = list(year = year,
-                                limit = limit,
-                                run_matches = run_matches
+                  params = list(year = 2019,
+                                limit = 15,
+                                run_matches = "no"
                                 ),
                   output_file = paste0("secret_santa_", year))
 
